1da652c9b47fca6776ea23da438c8d05a7a68946
Marcin Szymanek <202009418@post.au.dk>
2022-11-30 10:21:44 +0100

    added remaining queries, JBR prj file to gitignore

--- MongoDb/CommandCtrl.cs
+++ MongoDb/CommandCtrl.cs
@@ -10,15 +10,18 @@ public class CommandCtrl
 {
     private UI.UI _ui;
     private IMongoCollection<Facility> _facilities;
+    private IMongoCollection<User> _users;
     private readonly string connectionString = "mongodb://localhost:27017";
     private readonly string municipalityDB = "Municipalities";
     private readonly string facCollection = "Facilities";
+    private readonly string userCollection = "Users";
     public CommandCtrl(UI.UI ui)
     {
         _ui = ui;
         var client = new MongoClient(connectionString);
         var database = client.GetDatabase(municipalityDB);
         _facilities = database.GetCollection<Facility>(facCollection);
+        _users = database.GetCollection<User>(userCollection);
     }
 
     public void GetFacilitiesOrderByKind()
@@ -78,12 +81,74 @@ public class CommandCtrl
 
     public void GetReservations()
     {
+        string output;
+        _ui.Display("Getting a list of reservations\n\n");
+        var users = _users.Find(u => u.Reservations.Count > 0).ToList();
+        string header = "Booking User                         Facility                        Date and time\n";
+        _ui.Display(header);
+        foreach (var u in users)
+        {
+            foreach (var r in u.Reservations)
+            {
+                output = "";
+                output += u.CPR;
+                while (output.Length < "Booking User                         ".Length)
+                {
+                    output += " ";
+                }
 
+                output += r.Name;
+                
+                while (output.Length < header.Length - "Date and time\n".Length)
+                {
+                    output += " ";
+                }
+
+                output += r.Timeslot;
+                
+                _ui.Display(output + "\n");
+            }
+        }
     }
 
     public void GetReservationsWParticipants()
     {
+        string output;
+        _ui.Display("Getting a list of reservations\n\n");
+        var users = _users.Find(u => u.Reservations.Count > 0).ToList();
+        string header = "Booking User                         Facility                        Date and time\n";
+        _ui.Display(header);
+        foreach (var u in users)
+        {
+            foreach (var r in u.Reservations)
+            {
+                output = "";
+                output += u.CPR;
+                while (output.Length < "Booking User                         ".Length)
+                {
+                    output += " ";
+                }
 
+                output += r.Name;
+                
+                while (output.Length < header.Length - "Date and time\n".Length)
+                {
+                    output += " ";
+                }
+
+                output += r.Timeslot;
+                
+                _ui.Display(output + "\n");
+                _ui.Display("Participants: ");
+                foreach (var cpr in r.ParticipantCpr)
+                {
+                    _ui.Display(cpr + ", ");
+                }
+
+                _ui.Display("\n");
+            }
+            
+        }
     }
 
     public void GetMaintenanceHistory()
